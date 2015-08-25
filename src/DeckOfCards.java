f8928bd9ea3858e9fa2a7c7b2914b93b350693ea
Israel Castro <israel.casgon@gmail.com>
2015-08-24 20:59:38 -0500

    Base For Practice
    Base code to start the practice

---
+++ src/DeckOfCards.java
@@ -0,0 +1,26 @@
+
+public class DeckOfCards implements DeckOfCardsInterface {
+//THIS IS A TEST COMMENT
+	@Override
+	public void shuffle() {
+		
+	}
+
+	@Override
+	public int cardsLeft() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Card dealCard() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
