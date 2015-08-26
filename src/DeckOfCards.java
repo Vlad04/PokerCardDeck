10b3bc1872416e8ddb555d1b8bf9ad3bf9af745e
Vlad04 <Vlad04@users.noreply.github.com>
2015-08-26 04:44:00 +0200

    Update DeckOfCards.java

--- src/DeckOfCards.java
+++ src/DeckOfCards.java
@@ -1,15 +1,15 @@
 
 public class DeckOfCards implements DeckOfCardsInterface {
 
-	private Card[] deck;	
+	public Card[] deck;	
 	private int position;
 	
 	public DeckOfCards(){
 		this.deck=new Card[52];
 		int num=0;
-		for(short j=0;j<4;j++){
-			for(short i=0;i<13;i++){
-				this.deck[num]=new Card(i,j);		//falta el hacer el constructor de la carta para aceptar los argumentos
+		for(short j=1;j<=4;j++){
+			for(short i=1;i<=13;i++){
+				this.deck[num]=new Card(i,j);
 				num++;
 			}
 		}
@@ -20,7 +20,7 @@ public class DeckOfCards implements DeckOfCardsInterface {
 		int rnd;
 		Card temp;
 		for(int i=0;i<52;i++){
-			rnd= (int)(Math.random()*52);		//Genera número random entre 0 y 51
+			rnd= (int)(Math.random()*52);		//Genera nÃºmero random entre 0 y 51
 			temp= this.deck[i];				//Guarda el valor actual del naipe en un temporal
 			this.deck[i]=this.deck[rnd];	//Guarda el valor de un lugar random de naipe en el lugar actual
 			this.deck[rnd]=temp;				//Guarda el temporal en el lugar random
@@ -41,7 +41,20 @@ public class DeckOfCards implements DeckOfCardsInterface {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		DeckOfCards dc = new DeckOfCards();
 
+		for (int count = 0 ; count < 1 ; count ++){
+			
+			String tmp = dc.deck[count].toString();
+			System.out.println(tmp);
+		}
+		dc.shuffle();
+		System.out.println("\n despues del shufle\n");
+		for (int count = 0 ; count < 1 ; count ++){
+			
+			String tmp = dc.deck[count].toString();
+			System.out.println(tmp);
+		}
 	}
 
 }
